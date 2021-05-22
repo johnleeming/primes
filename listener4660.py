d807a1cf965af7c0521ed459f942f7533dfd577d
John Leeming <john@leeming.eu>
2021-05-22 12:20:19 +0100

    12ac

--- listener4660.py
+++ listener4660.py
@@ -14,25 +14,51 @@ def is_prime(num):
     return ans
 
 
-def find_primes(biggest):
-    ans = [1, 2, 3, 5, 7]
-    test = 11
+def find_primes(smallest, biggest):
+    test = smallest
+    lst = []
     while test < biggest:
         if is_prime(test):
-            ans.append(test)
+            lst.append(test)
         test += 2
-    return ans
+    return lst
 
 
 def find_tris(num):
     ans = []
     for n in range(num):
-        ans.append(int(n*(n+1)/2))
+        ans.append(int(n * (n + 1) / 2))
     return ans
 
 
+def int_to_list(num):
+    return list(map(int, str(num)))
+
+
+def ac12(ts, ps):
+    pos = []
+    for t in ts:
+        if 999 < t < 10000:
+            t_s = str(t)
+            for p in ps:
+                p_s = str(p)
+                i = 0
+                while i < len(t_s):
+                    i = t_s.find(p_s, i)
+                    if i == -1:
+                        break
+                    candidate = int(t_s[:i] + t_s[i + 2:])
+                    if candidate in ts:
+                        pos.append([t_s, p_s, str(candidate)])
+                    i += 1
+    return pos
+
+
 # main
-primes = find_primes(100)
-triangular_nos = find_tris(20)
+primes = find_primes(11, 99)
+triangular_nos = find_tris(200)
 print(primes)
 print(triangular_nos)
+ac12_pos = ac12(triangular_nos, primes)
+print(ac12_pos)
+
